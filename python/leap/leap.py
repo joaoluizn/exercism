34a7e16bbcb1cafe7b85748dda55fe21a95b3c06
John Luiz <joao.zenos@gmail.com>
2019-02-02 02:04:23 -0300

    Leap Question Solution

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,2 +1,16 @@
 def is_leap_year(year):
-    pass
+    """
+    :param int year: Given year to verify if its leap
+    :rtype: bool
+    """
+    # More legible solution
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            return False
+        return True
+    else:
+        return False
+    # Alternative solution in one line
+    # return True if year % 400 == 0 else False if year % 100 == 0 else True if year % 4 == 0 else False
