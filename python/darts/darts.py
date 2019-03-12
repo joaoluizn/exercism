a01df0c3890f0ac73129a9170e9ac22a05c38b0a
John Luiz <joao.zenos@gmail.com>
2019-03-12 19:57:36 -0300

    Darts Solution

---
+++ python/darts/darts.py
@@ -0,0 +1,12 @@
+import math
+
+def score(x, y):
+    distance = math.hypot(x, y)
+    if distance <= 1:
+        return 10
+    elif 1 < distance <= 5:
+        return 5
+    elif 5 < distance <= 10:
+        return 1
+    else:
+        return 0
