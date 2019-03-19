f336943d99a7cad72d0814d5730cc51dc735c8ab
John Luiz <joao.zenos@gmail.com>
2019-03-19 11:24:09 -0300

    Resistor Colors Solution

---
+++ python/side/resistor-colors/resistor_colors.py
@@ -0,0 +1,15 @@
+_COLORS = {
+    'black': 0,
+    'brown': 1,
+    'red': 2,
+    'orange': 3,
+    'yellow': 4,
+    'green': 5,
+    'blue': 6,
+    'violet': 7,
+    'grey': 8,
+    'white': 9,
+}
+
+def value(colors):
+    return int(''.join([str(_COLORS.get(k)) for k in colors]))
