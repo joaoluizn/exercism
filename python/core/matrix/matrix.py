ddde46b21ab2c0fa1ee5dbb9dd88b835cea02cbf
John Luiz <joao.zenos@gmail.com>
2019-04-10 20:31:54 -0300

    Matrix solution Patch 2

--- python/core/matrix/matrix.py
+++ python/core/matrix/matrix.py
@@ -1,12 +1,12 @@
 class Matrix(object):
     def __init__(self, matrix_string):
-        self.rows = [k.split() for k in matrix_string.split('\n')]
+        self.rows = [list(map(int, k.split())) for k in matrix_string.splitlines()]
 
     def row(self, index) -> list:
         """ Get row of desired index """
-        return list(map(int, self.rows[index-1]))
+        return self.rows[index-1]
 
     def column(self, index) -> list:
         """ Get column of desired index """
-        return [int(k[index-1]) for k in self.rows]
+        return [k[index-1] for k in self.rows]
 
