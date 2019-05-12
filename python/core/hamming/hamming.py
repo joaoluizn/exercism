f889379ca72922be0097bb3e11ec4a25ee70be1f
John Luiz <joao.zenos@gmail.com>
2019-05-12 18:07:54 -0300

    Hamming correction

--- python/core/hamming/hamming.py
+++ python/core/hamming/hamming.py
@@ -1,5 +1,4 @@
 def distance(strand_a :str, strand_b :str) -> int:
     if len(strand_a) != len(strand_b):
         raise ValueError("Strand Lenths doesn't match.")
-    else:
-        return len([k for k in zip(strand_a, strand_b) if k[0] != k[1]])
+    return len([(s1, s2) for s1, s2 in zip(strand_a, strand_b) if s1 != s2])
