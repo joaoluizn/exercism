c043ed1de96bd3bb48ff5aa64ada9654ac403285
John Luiz <joao.zenos@gmail.com>
2019-06-05 17:53:33 -0300

    Update Raindrops

--- python/core/raindrops/raindrops.py
+++ python/core/raindrops/raindrops.py
@@ -1,16 +1,5 @@
-""" Verify if 3, 5 and 7 are factors of a given number """
+""" Construct raindrop factors to a given number """
 def raindrops(number):
-    factors = [k for k in [3,5,7] if number % k == 0]
-    response = ''
-
-    if 3 in factors:
-        response += 'Pling'
-    if 5 in factors:
-        response += 'Plang'
-    if 7 in factors: 
-        response += 'Plong'
-
-    if not response:
-        response = f'{number}'
-
-    return response
+    drop_list = [(3, 'Pling'), (5, 'Plang'), (7, 'Plong')]
+    response = ''.join([drop for value, drop in drop_list if number % value == 0])
+    return response or str(number)
