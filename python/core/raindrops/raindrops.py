98f537bfab482c211d8d99c4cfa07dc37f80dbf5
John Luiz <joao.zenos@gmail.com>
2019-06-07 11:32:30 -0300

    Raindrops Update

--- python/core/raindrops/raindrops.py
+++ python/core/raindrops/raindrops.py
@@ -1,5 +1,9 @@
-""" Construct raindrop factors to a given number """
-def raindrops(number):
+""" Raindrops - Solution """
+
+def raindrops(number: int) -> str:
+    """
+    :param number: Number to be analyzed
+    """
     drop_list = [(3, 'Pling'), (5, 'Plang'), (7, 'Plong')]
     response = ''.join([drop for value, drop in drop_list if number % value == 0])
     return response or str(number)
