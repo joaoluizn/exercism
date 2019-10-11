4ed033bfe93bf2232900915df7b52f6df73a238a
Joao Luiz <joao.zenos@gmail.com>
2019-10-11 01:44:17 -0300

    Updating Isogram solution with generator

--- python/core/isogram/isogram.py
+++ python/core/isogram/isogram.py
@@ -1,11 +1,14 @@
 """ Isogram - Solution """
 
-from re import sub
-
 def is_isogram(string: str) -> bool:
     """
     :param string: String to be verified if isogram or not.
     """
-
-    string = sub(r'\W+', '', string).lower()
-    return len(set(string)) == len(string)
+    visited_letters = set()
+    word_generator = (k.lower() for k in string if k.isalpha())
+    for letter in word_generator:
+        if letter in visited_letters:
+            return False
+        else:
+            visited_letters.add(letter)
+    return True
